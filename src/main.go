7810b5fe8f67d9ed33d395e606febd2d4f4e9ef4
godotg <jaysunxiao@gmail.com>
2024-08-11 12:50:34 +0800

    update

--- src/main.go
+++ src/main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"fmt"
-	"gonet/base"
 )
 
 func main() {
 	fmt.Println("hello world")
-
-	//base.VarTest()
-	//base.NilTest()
-	//base.ConstTest()
-	//base.IfTest()
-	//base.ForTest()
-	//fmt.Println(base.Max(1, 2))
-	base.RoutinesTest()
 }
